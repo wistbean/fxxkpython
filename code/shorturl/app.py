04e961a47bf4d99930863a7c65a9e454cf9d363b
wistbean <wistbean.bb@gmail.com>
2021-09-12 01:34:17 +0800

    update short_url

--- code/shorturl/app.py
+++ code/shorturl/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, render_template, redirect
-from bases import Bases
+from flask import Flask, render_template, request, redirect
 import pymysql
+from bases import Bases
+
 
 bases = Bases()
 host = 'http://127.0.0.1:5000/'
+
 
 try:
     connection = pymysql.connect(host='localhost',
@@ -11,15 +13,15 @@ try:
                                  user='root',
                                  password='root',
                                  db='test',
-                                 charset='utf8')
+                                 charset='utf8'
+                                 )
 except pymysql.Error:
-    raise 
+    raise
 
 cursor = connection.cursor()
 
 
-app = Flask(__name__)
-
+app = Flask('__name__')
 
 @app.route('/')
 def index():
@@ -28,29 +30,29 @@ def index():
 
 @app.route('/gen_short_url', methods=['POST'])
 def gen_short_url():
-    long_url = request.form.get('long-url')
+    long_url = request.form.get('long_url')
+    # 将长链接存入数据库
     try:
-        cursor.execute("INSERT INTO urls (url) VALUES ('{}')".format(long_url))
+        cursor.execute('insert into urls (url) values ("{}")'.format(long_url))
         connection.commit()
     except pymysql.Error:
         raise
-    # 获取自增id
+    # 获取id
     last_id = cursor.lastrowid
+    # 将 id 转化为 62 进制
     encode = bases.toBase62(last_id)
-    short_url = host + encode
+    short_url = host + encode 
     return render_template('index.html', short_url=short_url)
 
 
-@app.route('/<encode_url>')
-def redirect_url(encode_url):
-    id = bases.fromBase62(encode_url)
+@app.route('/<encode_id>')
+def redirect_url(encode_id):
+    id = bases.fromBase62(encode_id)
     try:
-        cursor.execute("SELECT URL FROM urls WHERE id = " + str(id))
+        cursor.execute('select url from urls where id = {}'.format(id))
         connection.commit()
         url = cursor.fetchone()
-        return redirect(location=url[0])
-    except pymysql.Error as e:
-        print(str(e))
+        return redirect(url[0])
+    except pymysql.Error:
+        raise
 
-if __name__ == '__main__':
-    app.run(debug=True)
