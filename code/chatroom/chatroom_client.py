653e8de9b0a9bcc7636d9b898265ab433a9ef4bc
wistbean <wistbean.bb@gmail.com>
2022-01-09 11:36:46 +0800

    Update chatroom_client.py

--- code/chatroom/chatroom_client.py
+++ code/chatroom/chatroom_client.py
@@ -1,52 +1,73 @@
-from tkinter import * 
+from tkinter import *
 import socket
 from threading import Thread
 
-
 def get_msg():
+    # 获取消息传入到消息面板
     while True:
         try:
-            msg = s.recv(1024).decode('utf8')
-            text_message.insert(END, msg)
+            recv_msg = s.recv(1024).decode('utf8')
+            msg_text.insert(END, recv_msg + '\n')
+            msg_text.see(END)
         except:
             break
 
-
-def send():
-    send_msg = text_text.get('0.0', END)
-    s.send(bytes(send_msg, 'utf8'))
-    text_text.delete('0.0', END)
-
+def on_press_enter(event):
+    print('回车被按了一下')
+    msg = input_text.get('0.0', END)
+    # 发送消息到群里
+    s.send(bytes(msg, 'utf8'))
+    input_text.delete('0.0', END)
 
 root = Tk()
-root.title('帅帅的聊天室')
+root.title('深夜陪聊群')
 
-message_frame = Frame(width=480, height=300, bg='white')
-text_frame = Frame(width=480, height=100)
-send_frame = Frame(width=480, height=30)
+# 监听事件
+root.bind('<Return>', on_press_enter)
 
-text_message = Text(message_frame)
-text_text = Text(text_frame)
-button_send = Button(send_frame, text='发送', command=send)
+# 创建一个消息面板
+msg_frame = Frame(
+    root,
+    width=480,
+    height=390
+)
 
-message_frame.grid(row=0, column=0, padx=3, pady=6)
-text_frame.grid(row=1, column=0, padx=3, pady=6)
-send_frame.grid(row=2, column=0)
+# 创建一个输入面板
+input_frame = Frame(
+    root,
+    width=480,
+    height=100
+)
 
-message_frame.grid_propagate(0)
-text_frame.grid_propagate(0)
-send_frame.grid_propagate(0)
+msg_frame.grid_propagate(0)
+input_frame.grid_propagate(0)
 
-text_message.grid()
-text_text.grid()
-button_send.grid()
-# sockect 接入
-Host = '127.0.0.1'
-Port = 8080
+msg_text = Text(msg_frame)
+input_text = Text(input_frame)
 
+# 布局一下
+msg_frame.grid(
+    row=0,
+    padx=3,
+    pady=6
+)
+
+input_frame.grid(
+    row=1,
+    padx=3,
+    pady=6
+)
+
+msg_text.grid()
+input_text.grid()
+
+# 接入socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((Host, Port))
+s.connect(('127.0.0.1', 6666))
 
+# 获取服务端传来的信息
 receive_thread = Thread(target=get_msg)
 receive_thread.start()
+
+
 root.mainloop()
