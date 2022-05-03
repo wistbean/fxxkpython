1c50081d51093c43ade8e85dff157fc1d4b6459b
wistbean <wistbean.bb@gmail.com>
2022-05-04 00:01:49 +0800

    Update app.py

--- code/douyin_video_download/app.py
+++ code/douyin_video_download/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, Response, send_file
+from flask import Flask, render_template, request
 import requests
 from fake_headers import Headers
 import re
@@ -11,35 +11,26 @@ def index():
     return render_template('index.html')
 
 def get_video(video_url):
-    headers = Headers(os="mac", headers=True).generate()
-    req = requests.get(video_url, headers=headers, allow_redirects=True)
-    # print(req.url) https://www.iesdouyin.com/share/video/6967539188438813983
-    video_id = re.findall('video\/(\d+)\/', req.url)[0]
-    data_json = requests.get(f'https://www.iesdouyin.com/web/api/'
-                             f'v2/aweme/iteminfo/?item_ids={video_id}').content
-    video_addr = json.loads(data_json)['item_list'][0]['video']['play_addr']['url_list'][0]
-    video_addr = video_addr.replace('playwm', 'play') #去水印
-
-    path = f'static/cache/{video_id}.mp4'
-    r = requests.get(video_addr, headers=headers, stream=True)
-    with open(path, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-    return path
-
-
+    # 截取 https://v.douyin.com/FrsMefr/ 
+    url = re.findall(r'https://v.douyin.com/.*?/', video_url)[0]
+    # 获取重定向后的地址
+    headers = Headers(os='mac', headers=True).generate()
+    req = requests.get(url, headers=headers, allow_redirects=True)
+    # req.url: https://www.douyin.com/video/7072769079551069447?previous_page=web_code_link
+    video_id = re.findall('video\/(\d+)?', req.url)[0]
+    data = requests.get(f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={video_id}', headers=headers).text
+    video_address = json.loads(data)['item_list'][0]['video']['play_addr']['url_list'][0] 
+    # 去水印
+    video_address = video_address.replace('playwm', 'play')
+    return video_address 
 
 @app.route('/download_video', methods=['POST'])
 def gen_video():
-    video_url = request.form.get('video-url')
+    video_url = request.form.get('video_url')
     if video_url:
         video = get_video(video_url)
         return render_template('index.html', video=video)
-
-    return render_template('index.html', message='地址不能为空')
-
-
+    return render_template('index.html', msg='地址不能为空')
 
 if __name__ == '__main__':
     app.run(debug=True)
