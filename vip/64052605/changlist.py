62670fddadf295bfb241121facb611702352580b
KelvinWeng <64052605@qq.com>
2023-01-10 08:50:51 +0800

    add study not

---
+++ vip/64052605/changlist.py
@@ -0,0 +1,8 @@
+def change_list(list):
+    list = [1,'fxxk',3,4]
+    print (list)
+
+mylist = [1,2,3,4]
+mylist = [5,6,7,8]
+print(change_list(mylist))
+print(mylist)
---
+++ vip/64052605/sumtest.py
@@ -0,0 +1,7 @@
+def sum(a,b):
+    return (a+b)
+
+i = int(input("输入第一个数字"))
+j = int(input("输入第二个数字"))
+
+print (sum(i,j))
